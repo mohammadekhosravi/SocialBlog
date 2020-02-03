2d4d4170a7c4ee777d7832abf2b9b7778649a545
Mohammad Khosravi <mohammadkhosravi@birjand.ac.ir>
2020-02-03 14:28:29 +0330

    DB setup

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,6 +1,20 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+###############################################
+##############Database Setup###################
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite') 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+###############################################
 
 from blog.core.views import core
 from blog.error_handler.handlers import errors
