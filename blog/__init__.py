05fbb8370d5a3cbc5c6638df66af03169a06521a
Mohammad Khosravi <mohammadkhosravi@birjand.ac.ir>
2020-02-03 10:52:51 +0330

    Register errors blueprint

--- blog/__init__.py
+++ blog/__init__.py
@@ -3,4 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 from blog.core.views import core
+from blog.error_handler.handlers import errors
+
 app.register_blueprint(core)
+app.register_blueprint(errors)
