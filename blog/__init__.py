91e5fe2f5c0a17d38b7124dfd15d753fc078ea32
Mohammad Khosravi <mohammadkhosravi@birjand.ac.ir>
2020-02-05 14:56:12 +0330

    Add blog_posts/views

--- blog/__init__.py
+++ blog/__init__.py
@@ -28,7 +28,9 @@ login_manager.login_view = 'users.login'
 from blog.core.views import core
 from blog.error_handler.handlers import errors
 from blog.users.views import users
+from blog.blog_posts.views import blog_posts
 
 app.register_blueprint(core)
 app.register_blueprint(errors)
 app.register_blueprint(users)
+app.register_blueprint(blog_posts)
