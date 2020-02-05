2d5a987d189b7be58c59ceaf03025e943e7dae16
Mohammad Khosravi <mohammadkhosravi@birjand.ac.ir>
2020-02-05 16:38:04 +0330

    Fix some typo

--- blog/blog_posts/views.py
+++ blog/blog_posts/views.py
@@ -33,7 +33,7 @@ def blog_post(blog_post_id):
                            post=blog_post)
 
 # Update
-@blog_post.route('/<int:blog_post_id/update>', methods=['GET', 'POST'])
+@blog_posts.route('/<int:blog_post_id/update>', methods=['GET', 'POST'])
 @login_required
 def update(blog_post_id):
     blog_post = BlogPost.query.get_or_404(blog_post_id)
@@ -52,7 +52,7 @@ def update(blog_post_id):
         return redirect(url_for('blog_posts.blog_post',
                                 blog_post_id=blog_post.id))
 
-    elif request.method = "GET":
+    elif request.method == "GET":
         form.title.data = blog_post.title
         form.text.data = blog_post.text
 
@@ -61,7 +61,7 @@ def update(blog_post_id):
                            form=form)
 
 # Delete
-@blog_post.route('/<int:blog_post_id/delete>', methods=["GET", "POST"])
+@blog_posts.route('/<int:blog_post_id/delete>', methods=["GET", "POST"])
 @login_required
 def delete_post(blog_post_id):
 
